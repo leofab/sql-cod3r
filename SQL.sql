87e0c99aa21485197524ebc7b80602fcd73b541d
leofab <leofabr3@gmail.com>
2023-04-07 10:40:14 -0300

    inserting itens

--- SQL.sql
+++ SQL.sql
@@ -7,7 +7,7 @@ CREATE DATABASE starwars;
 
 USE starwars;
 
-CREATE TABLE IF NOT EXISTS personagem(
+CREATE TABLE IF NOT EXISTS personagens(
 	id INT,
 	nome VARCHAR(120),
 	altura DECIMAL(4, 2),
@@ -31,3 +31,30 @@ CREATE TABLE IF NOT EXISTS planetas(
 	qtde_luas INT,
 	habitado BOOLEAN
 )
+
+-- inserindo personagem
+
+INSERTO INTO IF NOT EXISTS
+	personagens (id, nome, altura, destaque, nascimento)
+VALUES
+	(1, 'Luke Skywalker', 1.75,	true, '3091-10-25'	)
+	
+INSERTO INTO IF NOT EXISTS
+	personagens (id, nome, altura, destaque, nascimento)
+VALUES
+	(2, 'Chewbacca', 2.05,	1, '3082-02-03'	)
+
+INSERTO INTO IF NOT EXISTS
+	personagens (id, nome, altura, destaque, nascimento)
+VALUES
+	(3, 'Darth Vadder', 1.95,	true, '3069-11-22'	)
+	
+INSERTO INTO IF NOT EXISTS
+	personagens (id, nome, altura, destaque, nascimento)
+VALUES
+	(4, 'Jabba', 1.65,	false, '3072-12-13'	)
+	
+INSERTO INTO IF NOT EXISTS
+	personagens (id, nome, altura, destaque, nascimento)
+VALUES
+	(5, 'Snoke', 2.15,	0, '3042-01-03'	)
